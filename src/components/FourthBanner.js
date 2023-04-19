fcebb4c8603dbe28bbe484ee71f2910ca378d46c
sklizzzkiy <127947819+sklizzzkiy@users.noreply.github.com>
2023-04-19 18:03:45 +0300

    fix text

--- src/components/FourthBanner.js
+++ src/components/FourthBanner.js
@@ -67,7 +67,7 @@ const FourthBanner = () => {
         <div className='banner fourth-banner' ref={fourthBannerRef}>
             <div className='fourth-banner__text'>
                 <h3>О проекте</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                <p>Мы готовы помочь сосредоточиться на себе и помочь раскрыть таланты и умения, которые Вы в себе не могли найти все это время</p>
             </div>
 
         </div>
